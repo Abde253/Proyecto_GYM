04dd22d8d1c2adf8e44c29ec12c0d57a9a269b46
David Mendoza <davmenmor2@alu.edu.gva.es>
2024-12-04 18:05:55 +0100

    ModificacionArrays4Diciembre

--- TarifasAsistente.java
+++ TarifasAsistente.java
@@ -1,2 +1,46 @@
 public class TarifasAsistente {
+    public void arraysTarifas(){
+        String[] tarifas = new String[6];
+        tarifas[0] = "Mensual";
+        tarifas[1] = "Anual";
+        tarifas[2] = "Personalizada";
+        tarifas[3] = "Diaria";
+        tarifas[4] = "Promociones";
+        tarifas[5] = "Premium";
+
+        String[] promociones = new String[3];
+        promociones[0] = "3 meses 5% descuento";
+        promociones[1] = "6 meses 10% descuento";
+        promociones[2] = "9 meses 15% descuento";
+
+        String[] formasPago = new String[3];
+        formasPago[0] = "Efectivo (Ticket de Pago)";
+        formasPago[1] = "Tarjeta de Credito";
+        formasPago[2] = "Plataformas de Pago";
+
+        String[] plataformasPago = new String[5];
+        plataformasPago[0] = "PayPal";
+        plataformasPago[1] = "Bizum";
+        plataformasPago[2] = "Bono Cultural";
+        plataformasPago[3] = "Paysafecard";
+        plataformasPago[4] = "CryptoMonedas";
+
+        String[] atencionCliente = new String[2];
+        atencionCliente[0] = "Preguntas Frecuentes";
+        atencionCliente[1] = "Comunicacion";
+
+        String[] preguntas = new String[2];
+        preguntas[0] = "Errores Comunes";
+        preguntas[1] = "Soporte Automatico";
+
+        String[] comunicacion = new String[3];
+        comunicacion[0] = "Numero de Telefono";
+        comunicacion[1] = "Correo Electronico";
+        comunicacion[2] = "Redes Sociales";
+
+        String[] redesSociales = new String[3];
+        redesSociales[0] = "Instagram";
+        redesSociales[1] = "Twitter";
+        redesSociales[2] = "Facebook";
+    }
 }
